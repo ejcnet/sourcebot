ba239c7fcee76b126ae34216c763cea567a2118b
paulmwatson <github.com@paulmwatson.com>
2017-07-29 00:49:31 +0200

    Remove dotenv

--- src/listener.php
+++ src/listener.php
@@ -1,15 +1,12 @@
 <?php
 require 'vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(__DIR__, '/../.env');
-$dotenv->load();
-
 use Mpociot\BotMan\BotManFactory;
 use React\EventLoop\Factory;
 
 $loop = Factory::create();
 $botman = BotManFactory::createForRTM([
-    'slack_token' => getenv('SLACK_BOT_USER_OAUTH_ACCESS_TOKEN')
+    'slack_token' => getenv("SLACK_BOT_USER_OAUTH_ACCESS_TOKEN")
 ], $loop);
 
 $botman->hears('ping', function($bot) {
