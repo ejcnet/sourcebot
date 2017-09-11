47fd671e31bcd14efc5274c24259fdd16f340722
paulmwatson <github.com@paulmwatson.com>
2017-09-11 15:56:32 +0200

    Remove try/catch

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -7,11 +7,8 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
+window.$ = window.jQuery = require('jquery');
+require('bootstrap-sass');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -33,8 +30,6 @@ let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
 /**
